1231c714b8770768735af02f2447a5676576c20b
Vipin Yadav <vyvipinyadav998@gmail.com>
2022-09-13 23:32:43 +0530

    shop app

---
+++ shop/models.py
@@ -0,0 +1,92 @@
+
+from tabnanny import verbose
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.db import models
+from django.db.models.signals import post_save
+from django.shortcuts import reverse
+
+
+# Create your models here.
+class Customer(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_pic= models.ImageField(upload_to='profile_pic/CustomerProfilePic/',null=True,blank=True)
+    address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20,null=False)
+    @property
+    def get_name(self):
+        return self.user.first_name+" "+self.user.last_name
+    @property
+    def get_id(self):
+        return self.user.id
+    def __str__(self):
+        return self.user.first_name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    Description = models.CharField(max_length=100,null=True, blank=True)
+    category = models.ForeignKey("Category", null=False,blank= False, on_delete=models.CASCADE)
+    original_price = models.FloatField(null=False, blank=False)
+    discount_price = models.FloatField(null=True, blank= True)
+    image = models.ImageField(upload_to='product_image', null = True, blank =True)
+    slug = models.SlugField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("core:product", kwargs={
+            'slug': self.slug
+        })
+
+    def get_add_to_cart_url(self):
+        return reverse("core:add-to-cart", kwargs={
+            'slug': self.slug
+        })
+
+    def get_remove_from_cart_url(self):
+        return reverse("core:remove-from-cart", kwargs={
+            'slug': self.slug
+        })
+
+class Category(models.Model):
+    category = models.CharField(max_length=50, null=False, blank= False)
+
+    def __str__(self) -> str:
+        return self.category
+
+    class Meta:
+        verbose_name = "Categorie"
+    
+
+class Order(models.Model):
+    STATUS =(
+        ('Pending','Pending'),
+        ('Order Confirmed','Order Confirmed'),
+        ('Out for Delivery','Out for Delivery'),
+        ('Delivered','Delivered'),
+    )
+    customer=models.ForeignKey('Customer', on_delete=models.CASCADE,null=True)
+    product=models.ForeignKey('Product',on_delete=models.CASCADE,null=True)
+    email = models.CharField(max_length=50,null=True)
+    address = models.CharField(max_length=500,null=True)
+    mobile = models.CharField(max_length=20,null=True)
+    order_date= models.DateField(auto_now_add=True,null=True)
+    status=models.CharField(max_length=50,null=True,choices=STATUS)
+
+class Feedback(models.Model):
+    name=models.CharField(max_length=40)
+    feedback=models.CharField(max_length=500)
+    date= models.DateField(auto_now_add=True,null=True)
+    def __str__(self):
+        return self.name
+
+
+
+# def userprofile_receiver(sender, instance, created, *args, **kwargs):
+#     if created:
+#         userprofile = UserProfile.objects.create(user=instance)
+
+
+# post_save.connect(userprofile_receiver, sender=settings.AUTH_USER_MODEL)
